ca41d92310c06b1451311584c9c779a2806f5446
Darren Yang <telunyang@gmail.com>
2022-07-01 02:42:48 +0800

    加入 plot ylim 範圍

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = './outputs/training_progress_scores.csv'
+path = './outputs_bert/training_progress_scores.csv'
 df = pd.read_csv(path)
 print(df)
 
@@ -9,5 +9,6 @@ plot = df.plot.line(x='global_step', y=['train_loss', 'eval_loss'])
 plt.title('NER')
 plt.ylabel('loss')
 plt.xlabel('step')
-plt.legend(['training', 'validation'], loc='lower right')
+plt.legend(['training', 'validation'], loc='upper right')
+plt.ylim(0.0, 1.0)
 plt.show()
