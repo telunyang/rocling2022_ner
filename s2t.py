7c72833aee2db912467651c06502e2bd0cd7223e
Darren Yang <telunyang@gmail>
2022-09-01 01:02:32 +0800

    保留 ccks 簡轉繁的功能

--- s2t.py
+++ s2t.py
@@ -1,8 +1,9 @@
 import opencc
 converter = opencc.OpenCC('s2tw.json')
+
+'''ccks'''
 with open('./dataset/ccks2017.json', 'r', encoding="utf-8") as file:
     s_content = file.read()
-
 t_content = converter.convert(s_content)
 with open("./dataset/ccks2017_m.json", "w", encoding="utf-8") as file:
     file.write(t_content)
