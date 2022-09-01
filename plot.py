57ecd99c92c76f562568db188c5c6fe903414996
Darren Yang <telunyang@gmail>
2022-09-01 09:21:40 +0800

    去掉程式輸出多餘的 checkpoint eval 結果

--- plot.py
+++ plot.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = './model_bert-base-chinese_T01/training_progress_scores.csv'
+path = './model_bert-base-chinese_train-test-label-standalone/training_progress_scores.csv'
 df = pd.read_csv(path)
-df['epoch'] = df.index + 1
-print(df)
+
+df = df[ df['global_step'] % 2000 != 0]
+df.insert(0, 'epoch', range(1, 1 + len(df)))
+# df['epoch'] = df.index + 1
+
+print(df[ ['epoch', 'precision', 'recall', 'f1_score'] ])
 
 plot = df.plot.line(x='epoch', y=['train_loss', 'eval_loss'])
 plt.title('NER')
