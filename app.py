d221de9f2f52d611e7f14117c7e17fcf647fea19
Darren Yang <telunyang@gmail>
2022-09-01 23:59:29 +0800

    修改 model 路徑

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ app = Flask(__name__)
 
 # 模型設定
 model_type = 'bert'
-output_dir = './model_bert-base-chinese_train_test_standalone/'
+
+'''
+./model_bert-base-chinese_train_test_standalone/
+./model_train-test-labelled-custom250sent_standalone
+'''
+output_dir = './model_train-test-labelled-custom250sent_standalone'
+
 model = NERModel(
     model_type, 
     output_dir,
