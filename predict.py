a54cefb022876c1f302e8896849ecea26c50c7ca
Darren Yang <telunyang@gmail>
2022-09-02 00:34:58 +0800

    測試 eval 的預測結果

--- predict.py
+++ predict.py
@@ -1,27 +1,47 @@
 # 匯入套件
 from simpletransformers.ner import NERModel
+from time import time
+from pprint import pprint
+import json
 
 # 模型路徑
 model_type = 'bert'
-output_dir = 'model_bert-base-chinese_train-test-label-standalone/'
+
+'''
+./model_bert-base-chinese_train_test_standalone/
+./model_train-test-labelled-custom250sent_standalone
+'''
+output_dir = './model_train-test-labelled-custom250sent_standalone'
+
+# 官方提供 eval 資料 (被轉換過，非原始資料)
+path_eval_data = './dataset/ROCLING22_CHNER_test.json'
 
 if __name__ == "__main__":
+    # 計算執行時間
+    time_s = time()
+
     # 模型設定
+    time_model = time()
     model = NERModel(
         model_type, 
         output_dir,
         use_cuda = True, 
         cuda_device = 0
     )
+    print(f"模型讀取時間: {time() - time_model} 秒 => {(time() - time_model) / 60} 分鐘")
 
     # 測試句子(可以有很多筆，都 append 在 list_sentences 當中)
-    list_sentences = [
-        '身體若有發炎較常導致該指數偏高，但若是病毒感染會合併發燒。',
-        '修復肌肉與骨骼最重要的便是熱量、蛋白質與鈣質。',
-        '如何治療胃食道逆流症？'
-    ]
+    time_read_eval_data = time()
+    with open(path_eval_data, "r", encoding="utf-8") as file:
+        list_sentences = json.loads( file.read() )
+    print(f"資料讀取時間: {time() - time_read_eval_data} 秒 => {(time() - time_read_eval_data) / 60} 分鐘")
 
     # 使用模型預測 (split_on_space 預設為 True，將英文單字間的空白當作分隔符號，但中文不需要，所以要改成 False)
-    predictions, raw_outputs = model.predict(list_sentences, split_on_space=False)
-    print("=== 預測結果 ===")
-    print(predictions)
+    time_predict = time()
+    predictions, raw_outputs = model.predict(list_sentences[:2], split_on_space=False)
+    print(f"預測執行時間: {time() - time_read_eval_data} 秒 => {(time() - time_read_eval_data) / 60} 分鐘")
+
+    pprint(predictions)
+
+
+    print(f"整體執行時間: {time() - time_s} 秒 => {(time() - time_s) / 60} 分鐘")
